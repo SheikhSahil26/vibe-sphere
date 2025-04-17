086fd94aa782e72ed645094d5e1892a219da2092
SheikhSahil26 <sahilsheikh261204@gmail.com>
2025-04-17 20:26:30 +0530

    corrected firebase configurations

--- backend/utils/firebaseConfig.js
+++ backend/utils/firebaseConfig.js
@@ -11,7 +11,8 @@ const serviceAccount = {
     auth_uri: process.env.FIREBASE_AUTH_URI,
     token_uri: process.env.FIREBASE_TOKEN_URI,
     auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
   };
 const databaseURL = process.env.FIREBASE_DATABASE_URL;
 
